bc3c87f81632363d505116e45dc3907e422047f7
Houssem <houssem@3dverse.com>
2024-05-30 17:59:08 -0400

    feat: mpr

--- livelink.samples/src/samples/quadruple-canvas/QuadrupleCanvas.tsx
+++ livelink.samples/src/samples/quadruple-canvas/QuadrupleCanvas.tsx
@@ -2,6 +2,89 @@
 import { useEffect, useRef } from "react";
 import Canvas from "../../components/Canvas";
 import { useLivelinkInstance } from "../../hooks/useLivelinkInstance";
+import { Camera, Quat } from "livelink.js";
+import { DefaultCamera } from "../../components/DefaultCamera";
+import CameraControls from "camera-controls";
+import * as THREE from "three";
+
+//------------------------------------------------------------------------------
+class MPRCamera extends Camera {
+    static i = 0;
+    cameraControls: CameraControls | null = null;
+    onCreate() {
+        const ar = this.viewport!.width / this.viewport!.height;
+
+        const orientations: Array<Quat> = [
+            [0.7071067811865475, 0, 0, 0.7071067811865476],
+            [0, 0.7071067811865475, 0, -0.7071067811865476],
+            [0, 0, 0, 1],
+        ];
+
+        this.local_transform = { position: [0, 0, 0], orientation: orientations[MPRCamera.i] };
+        this.camera = {
+            renderGraphRef: "c57253bf-40f2-44f1-942f-cc55dacea4f5",
+            dataJSON: {},
+        };
+        this.orthographic_lens = {
+            bottom: -1,
+            top: 1,
+            left: -ar,
+            right: ar,
+            zNear: -100,
+            zFar: 100,
+        };
+
+        this._initController();
+        ++MPRCamera.i;
+    }
+
+    private _initController() {
+        if (this.viewport === null) {
+            return;
+        }
+
+        const camera = new THREE.OrthographicCamera(
+            this.orthographic_lens!.left,
+            this.orthographic_lens!.right,
+            this.orthographic_lens!.bottom,
+            this.orthographic_lens!.top,
+            this.orthographic_lens!.zNear,
+            this.orthographic_lens!.zFar,
+        );
+        const clock = new THREE.Clock();
+        // create camera controls
+        const cameraControls = new CameraControls(camera, this.viewport.canvas);
+        this.cameraControls = cameraControls;
+
+        cameraControls.setOrbitPoint(0, 0, 0);
+        cameraControls.setPosition(...this.local_transform!.position!);
+        const t = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+        ][MPRCamera.i];
+        cameraControls.setPosition(t[0], t[1], t[2]);
+        cameraControls.setTarget(0, 0, 0);
+
+        cameraControls.addEventListener("update", () => this.onCameraUpdate());
+        // animate the camera
+        (function anim() {
+            const delta = clock.getDelta();
+            cameraControls.update(delta);
+            requestAnimationFrame(anim);
+        })();
+    }
+
+    onCameraUpdate() {
+        if (!this.cameraControls) return;
+        const cameraPosition = this.cameraControls.camera.position.toArray();
+        const cameraOrientation = new THREE.Quaternion();
+        this.cameraControls.camera.getWorldQuaternion(cameraOrientation);
+        const cameraOrientationArray = cameraOrientation.toArray();
+        this.local_transform!.position = cameraPosition;
+        this.local_transform!.orientation = cameraOrientationArray as Quat;
+    }
+}
 
 //------------------------------------------------------------------------------
 export default function QuadrupleCanvas() {
@@ -11,13 +94,14 @@ export default function QuadrupleCanvas() {
     const canvasRef4 = useRef<HTMLCanvasElement>(null);
 
     const { connect } = useLivelinkInstance({
-        canvas_refs: [canvasRef1, canvasRef2, canvasRef3, canvasRef4],
         token: "public_p54ra95AMAnZdTel",
+        canvas_refs: [canvasRef1, canvasRef2, canvasRef3, canvasRef4],
+        camera_constructors: [DefaultCamera, MPRCamera, MPRCamera, MPRCamera],
     });
 
     useEffect(() => {
         connect({
-            scene_id: "e7d69f14-d18e-446b-8df3-cbd24e10fa92",
+            scene_id: "34bddfef-cb5c-45ce-b4e4-5d271ba0dcf1",
         });
     }, []);
 
