81cf56b7868f4685e4dad887c3adce5deca685c5
Houssem <houssem@3dverse.com>
2024-06-06 12:57:59 -0400

    fix: collaborators sample

--- livelink.samples/src/samples/collaborators/Collaborators.tsx
+++ livelink.samples/src/samples/collaborators/Collaborators.tsx
@@ -1,7 +1,6 @@
 //------------------------------------------------------------------------------
 import { useEffect, useRef, useState } from "react";
 import Canvas from "../../components/Canvas";
-import { Button } from "react-daisyui";
 import { useLivelinkInstance } from "../../hooks/useLivelinkInstance";
 
 //------------------------------------------------------------------------------
@@ -36,29 +35,29 @@ export default function Collaborators() {
     }, [instance, setClients]);
 
     return (
-        <>
-            <div className="relative w-full h-full flex basis-full grow p-4">
-                <Canvas canvasRef={canvasRef} />
-                <div className="absolute right-6 top-6">
-                    <div className="avatar-group -space-x-6 rtl:space-x-reverse">
-                        {clients.map(client_id => (
-                            <div key={client_id} className="avatar">
-                                <div className="w-12">
-                                    <img
-                                        title={client_id}
-                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                                    />
-                                </div>
+        <div className="relative h-full max-h-screen p-3">
+            <Canvas canvasRef={canvasRef} />
+            <div
+                className={`absolute ${instance ? "top-6 left-6" : "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"}`}
+            >
+                <button className="button button-primary" onClick={toggleConnection}>
+                    {instance ? "Disconnect" : "Connect"}
+                </button>
+            </div>
+            <div className="absolute right-6 top-6">
+                <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+                    {clients.map(client_id => (
+                        <div key={client_id} className="avatar">
+                            <div className="w-12">
+                                <img
+                                    title={client_id}
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                                />
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <div className="flex items-center gap-2 pb-4">
-                <Button shape="circle" variant="outline" onClick={toggleConnection}>
-                    {instance ? "Disconnect" : "Connect"}
-                </Button>
-            </div>
-        </>
+        </div>
     );
 }
