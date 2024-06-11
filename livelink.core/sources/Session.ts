ce70fb7010bb866486f2337ae79ad9a8df428ec7
Mark Downey <mark@3dverse.com>
2024-06-11 15:48:05 -0400

    fix: use dev api url

--- livelink.core/sources/Session.ts
+++ livelink.core/sources/Session.ts
@@ -1,7 +1,7 @@
 import type { UUID } from "./types";
 import { Client } from "./Client";
 
-const api_url = "https://api.3dverse.com/app/v1";
+const api_url = "https://api.3dverse.dev/app/v1";
 
 /**
  *
