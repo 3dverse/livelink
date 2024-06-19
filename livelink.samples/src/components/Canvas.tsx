a5d1cea71707b3b34764613fac611dd05a993daa
Baptiste Macaire <baptiste@3dverse.com>
2024-06-19 15:50:21 -0400

    style: increase canvas contrast

--- livelink.samples/src/components/Canvas.tsx
+++ livelink.samples/src/components/Canvas.tsx
@@ -14,7 +14,7 @@ export default function Canvas({
             onContextMenu={event => event.preventDefault()}
             style={{ width: "100%", height: "100%" }}
             tabIndex={1}
-            className={`max-h-screen bg-color-overground rounded-xl ${className}`}
+            className={`max-h-screen bg-[#1e222e] rounded-xl ${className}`}
         ></canvas>
     );
 }
