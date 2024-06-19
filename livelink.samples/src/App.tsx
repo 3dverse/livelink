f6407066a96933b20a8d26e2ff7cf9886cbd8c2a
Baptiste Macaire <baptiste@3dverse.com>
2024-06-19 15:47:34 -0400

    fix: add a scrollbar to the main menu

--- livelink.samples/src/App.tsx
+++ livelink.samples/src/App.tsx
@@ -1,9 +1,10 @@
 //------------------------------------------------------------------------------
 import { useEffect, useState } from "react";
-import { NavLink, Outlet, useOutlet } from "react-router-dom";
-import { SAMPLES } from "./samples";
+import { Outlet, useOutlet } from "react-router-dom";
+
 import { Home } from "./components/Home";
 import { BarsIcon } from "./components-system/common/icons/BarsIcon";
+import { MainMenu } from "./components/MainMenu";
 
 //------------------------------------------------------------------------------
 function App() {
@@ -23,55 +24,7 @@ function App() {
     //------------------------------------------------------------------------------
     return (
         <div className="flex h-screen">
-            <div>
-                {isNavOpen && (
-                    <div
-                        className={`lg:hidden absolute top-0 left-0 w-screen h-full bg-color-underground transition-opacity opacity-0 cursor-pointer z-10 ${isNavOpen ? "opacity-80" : ""}`}
-                        onClick={() => setnavOpen(false)}
-                    />
-                )}
-                <nav
-                    className={`absolute lg:relative top-0 transition-transform h-screen ${isNavOpen ? "" : "-translate-x-full lg:translate-x-0"}`}
-                >
-                    <div
-                        className="relative bg-color-ground w-80 h-full px-5 py-4 shadow-2xl lg:shadow-none z-10"
-                        onClick={() => setnavOpen(false)}
-                    >
-                        <header className="mt-1 mb-6">
-                            <NavLink to="/" className="flex items-center gap-3 pl-3 font-primary text-lg">
-                                <img
-                                    src="https://console.3dverse.com/static/logo/3dverse-wordmark.svg"
-                                    className="h-6"
-                                />
-                                3dverse Samples
-                            </NavLink>
-                        </header>
-                        <ul className="flex flex-col gap-6 h-full text-color-secondary">
-                            {SAMPLES.map((category, i) => (
-                                <li key={i}>
-                                    <p className="mb-1 pl-4 text-2xs uppercase text-color-tertiary opacity-80 tracking-wider">
-                                        {category.categoryName}
-                                    </p>
-                                    {category.list.map((s, y) => (
-                                        <NavLink
-                                            key={y}
-                                            to={s.path}
-                                            className={({ isActive }) =>
-                                                [
-                                                    "button button-ghost text-sm justify-start rounded-xl",
-                                                    isActive ? "active" : "",
-                                                ].join(" ")
-                                            }
-                                        >
-                                            {s.title}
-                                        </NavLink>
-                                    ))}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+            <MainMenu isOpen={isNavOpen} onClose={() => setnavOpen(false)} />
             <div className="grow">
                 {outlet ? <Outlet /> : <Home />}
                 <button
---
+++ livelink.samples/src/components/MainMenu.tsx
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+import { NavLink } from "react-router-dom";
+import { SAMPLES } from "../samples";
+
+//------------------------------------------------------------------------------
+export function MainMenu({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+    return (
+        <div>
+            {isOpen && (
+                <div
+                    className={`
+                        lg:hidden absolute top-0 left-0 w-screen h-full bg-color-underground transition-opacity opacity-0 cursor-pointer z-10
+                        ${isOpen ? "opacity-80" : ""}
+                    `}
+                    onClick={onClose}
+                />
+            )}
+
+            <nav
+                className={`absolute lg:relative top-0 transition-transform h-screen ${isOpen ? "" : "-translate-x-full lg:translate-x-0"}`}
+            >
+                <div
+                    className="relative w-80 h-full flex flex-col bg-color-ground shadow-2xl lg:shadow-none z-10"
+                    onClick={onClose}
+                >
+                    <header className="my-6 px-5">
+                        <NavLink to="/" className="flex items-center gap-3 pl-3 font-primary text-lg">
+                            <img src="https://console.3dverse.com/static/logo/3dverse-wordmark.svg" className="h-6" />
+                            3dverse Samples
+                        </NavLink>
+                    </header>
+                    <ul className="flex flex-col gap-6 h-full px-5 py-4 text-color-secondary overflow-auto">
+                        {SAMPLES.map((category, i) => (
+                            <li key={i}>
+                                <p className="mb-1 pl-4 text-2xs uppercase text-color-tertiary opacity-80 tracking-wider">
+                                    {category.categoryName}
+                                </p>
+                                {category.list.map((s, y) => (
+                                    <NavLink
+                                        key={y}
+                                        to={s.path}
+                                        className={({ isActive }) =>
+                                            [
+                                                "button button-ghost text-sm justify-start rounded-xl",
+                                                isActive ? "active" : "",
+                                            ].join(" ")
+                                        }
+                                    >
+                                        {s.title}
+                                    </NavLink>
+                                ))}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
