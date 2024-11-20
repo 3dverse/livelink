3c911f62a80e437eb44ebd433e7d2cc62b8a43a6
Alexandre Petit <alex@3dverse.com>
2024-11-19 22:58:21 -0500

    feat: init livelink-react-ui

---
+++ livelink.react.ui/esbuild.js
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+const pkg = require("./package.json");
+const esbuild = require("esbuild");
+
+//------------------------------------------------------------------------------
+const commonBuildOptions = {
+    entryPoints: ["./sources/index.ts"],
+    outdir: "dist",
+    bundle: true,
+    minify: true,
+    platform: "browser",
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    sourcemap: true,
+};
+
+//------------------------------------------------------------------------------
+const buildOptions = [
+    {
+        format: "esm",
+        outExtension: { ".js": ".mjs" },
+    },
+    {
+        format: "cjs",
+        outExtension: { ".js": ".cjs" },
+    },
+];
+
+//------------------------------------------------------------------------------
+const devBuildOptions = {
+    ...commonBuildOptions,
+    ...buildOptions[0],
+    minify: false,
+};
+
+//------------------------------------------------------------------------------
+(async () => {
+    if (process.argv.includes("dev")) {
+        const ctx = await esbuild.context(devBuildOptions);
+        await ctx.watch();
+        return;
+    }
+
+    for (const options of buildOptions) {
+        console.log(`Building ${options.format}...`);
+        await esbuild.build({ ...commonBuildOptions, ...options });
+    }
+})();
