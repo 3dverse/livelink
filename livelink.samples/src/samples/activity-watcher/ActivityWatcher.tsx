7b7eea3b08bd7c24baf1a56c0edc1ef74b437fe4
Baptiste Macaire <baptiste@3dverse.com>
2024-12-09 13:53:29 +0000

    fix: handle inactivity with component InactivityWarning

--- livelink.samples/src/samples/activity-watcher/ActivityWatcher.tsx
+++ livelink.samples/src/samples/activity-watcher/ActivityWatcher.tsx
@@ -11,14 +11,12 @@ export default function SimpleCanvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     const { instance, connect, disconnect } = useLivelinkInstance({ views: [{ canvas_ref: canvasRef }] });
-    const [inactivityWarning, setInactivityWarning] = useState<boolean>(false);
     const [isDisconnected, setIsDisconnected] = useState<boolean>(false);
     const [isDisconnectedForInactivity, setIsDisconnectedForInactivity] = useState<boolean>(false);
 
     const toggleConnection = async () => {
         if (instance) {
             disconnect();
-            instance!.activity_watcher.removeEventListener("on-warning", onInactivityWarning);
         } else if (canvasRef.current) {
             connect({
                 scene_id: "15e95136-f9b7-425d-8518-d73dab5589b7",
@@ -28,15 +26,6 @@ export default function SimpleCanvas() {
         }
     };
 
-    function onInactivityWarningValidated() {
-        instance!.activity_watcher.reset();
-        setInactivityWarning(false);
-    }
-
-    function onInactivityWarning() {
-        setInactivityWarning(true);
-    }
-
     function onDisconnected(event: Event) {
         const reason: GatewayDisconnectedReason = (event as CustomEvent).detail.reason;
         setIsDisconnected(true);
@@ -44,7 +33,6 @@ export default function SimpleCanvas() {
     }
 
     async function onConnected({ instance }: { instance: Livelink }) {
-        instance.activity_watcher.addEventListener("on-warning", onInactivityWarning);
         instance.session.addEventListener("on-disconnected", onDisconnected);
     }
 
@@ -64,7 +52,7 @@ export default function SimpleCanvas() {
                 <div className="absolute top-0 bottom-0 left-0 right-0 p-3 content-center">
                     <div className="flex flex-col items-center justify-center gap-3">
                         <div className="text-sm">
-                            {`You've been disconnected${isDisconnectedForInactivity ? " for inactivity" : ""}`}
+                            You've been disconnected{isDisconnectedForInactivity ? " for inactivity" : ""}.
                         </div>
                     </div>
                 </div>
