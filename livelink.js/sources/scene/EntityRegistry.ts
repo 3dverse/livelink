84f2cfdecb5bbbbc213cd7213b26a156921e3bbd
Houssem <houssem@3dverse.com>
2025-01-02 10:24:22 -0500

    refactor: removed default values handling fron entity registry

--- livelink.js/sources/scene/EntityRegistry.ts
+++ livelink.js/sources/scene/EntityRegistry.ts
@@ -3,7 +3,6 @@ import type {
     RTID,
     UUID,
     UpdateComponentsCommand,
-    ComponentDescriptor,
     ComponentsRecord,
     ComponentType,
     ComponentName,
@@ -52,11 +51,6 @@ export class EntityRegistry {
      * List of dirty entities that need to be broadcasted to the editor sorted by component type.
      */
     #dirty_components_to_broadcast = new Map<ComponentName, Set<Entity>>();
-
-    /**
-     * Default values for all component attributes.
-     */
-    #component_default_values = new Map<ComponentName, object>();
 
     /**
      *
@@ -164,44 +158,6 @@ export class EntityRegistry {
         for (const entity of this.#entities) {
             visitor(entity);
         }
-    }
-
-    /**
-     * @internal
-     * Configures the default values for all component attributes.
-     *
-     * @param component_descriptors The component descriptors to use.
-     */
-    _configureComponentDefaultValues({
-        component_descriptors,
-    }: {
-        component_descriptors: Record<ComponentName, ComponentDescriptor>;
-    }): void {
-        for (const key in component_descriptors) {
-            const component_name = key as ComponentName;
-            const defaultValue = {} as Record<string, unknown>;
-            const component_descriptor = component_descriptors[component_name];
-            for (const attribute of component_descriptor.attributes) {
-                if (attribute.mods?.indexOf("engine-only") > -1) {
-                    continue;
-                }
-
-                if (attribute.default !== undefined) {
-                    defaultValue[attribute.name] = attribute.default;
-                } else {
-                    defaultValue[attribute.name] = get_attribute_default_value(attribute.type);
-                }
-            }
-
-            this.#component_default_values.set(component_name, defaultValue);
-        }
-    }
-
-    /**
-     * @internal
-     */
-    _getComponentDefaultValue({ component_type }: { component_type: ComponentName }): object {
-        return this.#component_default_values.get(component_type) ?? {};
     }
 
     /**
@@ -362,64 +318,4 @@ export class EntityRegistry {
             entities.clear();
         }
     }
-}
-
-/**
- * @internal
- */
-const INVALID_UUID = "00000000-0000-0000-0000-000000000000" as const;
-
-/**
- * @internal
- */
-function get_attribute_default_value(type: string): unknown {
-    if (type.startsWith("array")) {
-        return [];
-    }
-
-    if (type.startsWith("map<")) {
-        return {};
-    }
-
-    switch (type) {
-        case "string":
-            return "";
-        case "json":
-            return {};
-        case "entity_ref":
-            return { originalEUID: INVALID_UUID, linkage: [] };
-        case "uuid":
-            return INVALID_UUID;
-        case "bool":
-            return false;
-        case "float":
-            return 0;
-    }
-
-    if (type.endsWith("_ref")) {
-        return INVALID_UUID;
-    }
-
-    if (type.includes("int")) {
-        return 0;
-    }
-
-    const vectorMatch = type.match(/^i?vec([2-4])$/);
-    if (vectorMatch) {
-        return Array.from(Array(parseInt(vectorMatch[1]))).map(() => 0);
-    }
-
-    const matrixMatch = type.match(/^mat([2-4])$/);
-    if (matrixMatch) {
-        const matrixSize = parseInt(matrixMatch[1]);
-        const elementCount = matrixSize * matrixSize;
-        const result = Array.from(Array(elementCount)).map(() => 0);
-        for (let i = 0; i < matrixSize; ++i) {
-            result[i * matrixSize + i] = 1;
-        }
-        return result;
-    }
-
-    console.warn(`Unrecognized attribute type : ${type}`);
-    return undefined;
 }
