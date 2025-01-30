434d8b4c0a467fd260097c34a591b8b59f2cb3d2
Houssem <houssem@3dverse.com>
2025-01-30 12:49:38 -0500

    fix(livelink.samples): MPR sample fully functional with orthographic projections

--- livelink.samples/src/samples/advanced/x-multiplanar-reconstruction.tsx
+++ livelink.samples/src/samples/advanced/x-multiplanar-reconstruction.tsx
@@ -10,6 +10,7 @@ import { LoadingOverlay } from "@3dverse/livelink-react-ui";
 
 //------------------------------------------------------------------------------
 import { DisconnectedModal } from "../../components/SamplePlayer";
+import { Vec2, Vec3 } from "@3dverse/livelink";
 
 //------------------------------------------------------------------------------
 const scene_id = "59705a36-56ed-49b3-b4fc-4b6cd69eb82c";
@@ -40,53 +41,63 @@ function App() {
 
 //------------------------------------------------------------------------------
 function AppLayout() {
-    const { cameraEntity: cameraEntity1 } = useCameraEntity({
-        position: [0, 0, 1.5],
-        orientation: [0.924, 0.383, 0, 0],
-        settings: { voxelMultiplier: 0.5, displayBackground: false },
-    });
-    const { cameraEntity: cameraEntity2 } = useCameraEntity({
-        renderGraphRef: "c57253bf-40f2-44f1-942f-cc55dacea4f5",
-    });
-
     return (
         <div className="flex basis-full p-2 gap-2 flex-row">
             <div className="flex basis-full gap-2 flex-col">
-                <Canvas className="w-full h-full">
-                    <Viewport
-                        cameraEntity={cameraEntity1}
-                        className="w-full h-full"
-                    >
-                        <CameraController />
-                    </Viewport>
-                </Canvas>
-                <Canvas className="w-full h-full">
-                    <Viewport
-                        cameraEntity={cameraEntity2}
-                        className="w-full h-full"
-                    >
-                        <CameraController />
-                    </Viewport>
-                </Canvas>
+                <VoxelView />
+                <MPRView position={[0, 0, 0.1]} eulerOrientation={[0, 0, 0]} />
             </div>
             <div className="flex basis-full gap-2 flex-col">
-                <Canvas className="w-full h-full">
-                    <Viewport
-                        cameraEntity={cameraEntity2}
-                        className="w-full h-full"
-                    >
-                        <CameraController />
-                    </Viewport>
-                </Canvas>
-                <Canvas className="w-full h-full">
-                    <Viewport
-                        cameraEntity={cameraEntity2}
-                        className="w-full h-full"
-                    >
-                        <CameraController />
-                    </Viewport>
-                </Canvas>
+                <MPRView position={[0, 0.1, 0]} eulerOrientation={[90, 0, 0]} />
+                <MPRView position={[0.1, 0, 0]} eulerOrientation={[0, 90, 0]} />
             </div>
         </div>
+    );
+}
+
+//------------------------------------------------------------------------------
+function VoxelView() {
+    const { cameraEntity } = useCameraEntity({
+        position: [0, 0, 1.75],
+        eulerOrientation: [90, 0, 0],
+        settings: { voxelStepMultiplier: 0.5, displayBackground: false },
+    });
+
+    return (
+        <Canvas className="w-full h-full">
+            <Viewport cameraEntity={cameraEntity} className="w-full h-full">
+                <CameraController />
+            </Viewport>
+        </Canvas>
+    );
+}
+
+//------------------------------------------------------------------------------
+function MPRView({
+    position,
+    eulerOrientation,
+}: {
+    position: Vec3;
+    eulerOrientation: Vec3;
+}) {
+    const MPR_RENDERGRAPH_UUID = "deba68af-4bed-4fde-9058-8cacfabe7e9f";
+    const orthographic_lens = {
+        zoomFactor: [0.6, 0.6] as Vec2,
+        zNear: 0,
+        zFar: 100,
+    };
+    const { cameraEntity } = useCameraEntity({
+        renderGraphRef: MPR_RENDERGRAPH_UUID,
+        position,
+        eulerOrientation,
+        orthographic_lens,
+    });
+
+    return (
+        <Canvas className="w-full h-full">
+            <Viewport cameraEntity={cameraEntity} className="w-full h-full">
+                <CameraController />
+            </Viewport>
+        </Canvas>
     );
 }
