0295a672a8698e5db2ea53e8c423577c5ddb64b3
Alexandre Petit <alex@3dverse.com>
2025-03-14 11:29:36 -0400

    feat(livelink.react): DOMEntity now react to the entity visibility state

--- livelink.react/sources/components/overlays/DOMEntity.tsx
+++ livelink.react/sources/components/overlays/DOMEntity.tsx
@@ -24,6 +24,7 @@ export function DOMEntity({
     scaleFactor?: number;
 }>): JSX.Element | null {
     const [worldPosition, setWorldPosition] = useState<Vec3>(entity ? entity.global_transform.position : [0, 0, 0]);
+    const [isVisible, setIsVisible] = useState(entity ? entity.is_visible : false);
 
     useEffect(() => {
         if (!entity) {
@@ -31,17 +32,21 @@ export function DOMEntity({
         }
 
         const updatePosition = (): void => setWorldPosition(entity.global_transform.position);
+        const updateVisibility = (): void => setIsVisible(entity.is_visible);
 
-        // Initialize the position
+        // Initialize states
         updatePosition();
+        updateVisibility();
 
         entity.addEventListener("on-entity-updated", updatePosition);
+        entity.addEventListener("on-entity-visibility-changed", updateVisibility);
         return (): void => {
             entity.removeEventListener("on-entity-updated", updatePosition);
+            entity.removeEventListener("on-entity-visibility-changed", updateVisibility);
         };
     }, [entity]);
 
-    if (!entity) {
+    if (!entity || !isVisible) {
         return null;
     }
 
