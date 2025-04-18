840d1ed3bc8697c158be276e13047896f58d0c74
Alexandre Petit <alex@3dverse.com>
2025-04-18 11:46:17 -0400

    chore(livelink.js): update livelink-core version

--- livelink.js/sources/types/constants.d.ts
+++ livelink.js/sources/types/constants.d.ts
@@ -1,3 +1,17 @@
-declare const LIVELINK_CORE_URL: string;
+/**
+ * Hostname of the API server, injected by the build system.
+ * @internal
+ */
 declare const API_HOSTNAME: string;
-declare const EDITOR_URL: string;
+
+/**
+ * Version of the Livelink library, injected by the build system.
+ * @internal
+ */
+declare const LIVELINK_VERSION: string;
+
+/**
+ * Name of the package, injected by the build system.
+ * @internal
+ */
+declare const PACKAGE_NAME: string;
