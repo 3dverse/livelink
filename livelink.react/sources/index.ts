2e411bd397e32018096e9dc344906fb3c996f90e
Alexandre Petit <alex@3dverse.com>
2025-04-19 23:23:00 -0400

    feat(livelink-react): add recorder component

--- livelink.react/sources/index.ts
+++ livelink.react/sources/index.ts
@@ -6,6 +6,7 @@ export * from "./components/core/Livelink";
 export * from "./components/core/Canvas";
 export * from "./components/core/Viewport";
 export * from "./components/core/CameraController";
+export * from "./components/utils/Recorder";
 export { CameraController as DefaultCameraController } from "@3dverse/livelink";
 
 export * from "./components/overlays/DOM3DOverlay";
