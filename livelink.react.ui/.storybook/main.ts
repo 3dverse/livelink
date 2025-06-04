23b66140acffefbd10ff9b8768fba5721a75c239
Houssem <houssem@3dverse.com>
2025-06-04 10:54:53 -0400

    chore: update package-lock mess

--- livelink.react.ui/.storybook/main.ts
+++ livelink.react.ui/.storybook/main.ts
@@ -1,26 +1,10 @@
-//------------------------------------------------------------------------------
 import type { StorybookConfig } from "@storybook/react-vite";
-import { join, dirname } from "path";
 
-//------------------------------------------------------------------------------
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-    return dirname(require.resolve(join(value, "package.json")));
-}
-
-//------------------------------------------------------------------------------
 const config: StorybookConfig = {
     stories: ["../docs/**/*.mdx", "../sources/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: [
-        getAbsolutePath("@storybook/addon-onboarding"),
-        getAbsolutePath("@chromatic-com/storybook"),
-        getAbsolutePath("@storybook/addon-docs")
-    ],
+    addons: ["@storybook/addon-onboarding", "@chromatic-com/storybook", "@storybook/addon-docs"],
     framework: {
-        name: getAbsolutePath("@storybook/react-vite"),
+        name: "@storybook/react-vite",
         options: {},
     },
     typescript: {
@@ -37,4 +21,5 @@ const config: StorybookConfig = {
         };
     },
 };
+
 export default config;
