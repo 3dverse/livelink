4e5d99e3f448a7d704ba9bd8021f3abb60f220ec
Baptiste Macaire <baptiste.mac@gmail.com>
2025-10-25 00:01:37 +0100

    style(livelink.samples): auto scroll to active menu item

---
+++ livelink.samples/src/components/common/ScrollableArea.tsx
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+import { ReactNode } from "react";
+
+//------------------------------------------------------------------------------
+export const ScrollableArea = ({
+    ref,
+    children,
+    className,
+    ...divProps
+}: { ref: React.RefObject<HTMLDivElement>; children: ReactNode; className?: string } & React.HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <div ref={ref} className={`scrollable-area ${className ?? ""}`} {...divProps}>
+            <div className="scrollable-up" />
+            {children}
+            <div className="scrollable-down" />
+        </div>
+    );
+};
