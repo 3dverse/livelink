cfa98b4769172c1c579dae7e13b6401304fe5fe6
Alexandre Petit <alex@3dverse.com>
2025-10-30 14:15:04 -0400

    feat(livelink.react): useCameraEntity now use scene default setting for the camera

--- livelink.react/sources/hooks/useCameraEntity.ts
+++ livelink.react/sources/hooks/useCameraEntity.ts
@@ -1,7 +1,17 @@
 //------------------------------------------------------------------------------
-import type { Vec3, Quat, RenderGraphDataObject, Entity, UUID, ComponentName, Components } from "@3dverse/livelink";
-import { useEntity } from "./useEntity";
+import type {
+    Vec3,
+    Quat,
+    RenderGraphDataObject,
+    Entity,
+    UUID,
+    Components,
+    ComponentsManifest,
+} from "@3dverse/livelink";
 import { StrictUnion } from "../utils";
+import { useSceneSettings } from "./useSceneSettings";
+import { useContext, useEffect, useState } from "react";
+import { LivelinkContext } from "../components/core/Livelink";
 
 /**
  * An exclusive union of the perspective and orthographic lenses.
@@ -84,53 +94,134 @@ export type UseCameraEntityProps = {
     RenderGraphProps;
 
 /**
+ * @internal
+ */
+const INVALID_RENDER_GRAPH_UUID = "00000000-0000-0000-0000-000000000000";
+
+/**
+ * @internal
+ */
+const DEFAULT_RENDER_GRAPH_UUID = "398ee642-030a-45e7-95df-7147f6c43392";
+
+/**
+ * @internal
+ */
+const DEFAULT_RENDER_GRAPH_SETTINGS = { grid: true, skybox: false, gradient: true };
+
+/**
+ * @internal
+ */
+const DEFAULT_PERSPECTIVE_LENS = { fovy: 60, nearPlane: 0.1, farPlane: 10000 };
+
+/**
+ *
+ */
+type EntityProps = { name: string; components: ComponentsManifest } | null;
+
+/**
  * A hook that creates a camera entity.
  *
  * @param props - The properties used to create the camera entity.
- * @param watchedComponents - The components to watch for changes. If any component in this list changes,
- * the entity trigger a redraw. If set to "any", the entity will trigger a redraw on any component change.
+ * Once created, the camera entity will persist with this properties until
+ * the component using this hook is unmounted.
  *
  * @returns The camera entity and a boolean indicating if the entity is pending creation.
  *
  * @category Hooks
  */
-export function useCameraEntity(
-    props: UseCameraEntityProps = {},
-    watchedComponents: Array<ComponentName> | "any" = [],
-): {
+export function useCameraEntity(props: UseCameraEntityProps = {}): {
     isPending: boolean;
     cameraEntity: Entity | null;
 } {
-    const {
-        name = "Camera",
-        position = [0, 1, 5],
-        orientation: _orientation = [0, 0, 0, 1],
-        eulerOrientation: _eulerOrientation = [0, 0, 0],
-        renderGraphRef = "398ee642-030a-45e7-95df-7147f6c43392",
-        settings = { grid: true, skybox: false, gradient: true },
-        renderTargetIndex = -1,
-        perspective_lens: _perspective_lens = { fovy: 60, nearPlane: 0.1, farPlane: 10000 },
-    } = props;
+    const { instance } = useContext(LivelinkContext);
+    const { sceneSettings } = useSceneSettings();
 
-    const orientation = "orientation" in props ? _orientation : undefined;
-    const eulerOrientation = !orientation && "eulerOrientation" in props ? _eulerOrientation : undefined;
+    const [cameraEntity, setCameraEntity] = useState<Entity | null>(null);
+    const [isPending, setIsPending] = useState(true);
 
-    const orthographic_lens = "orthographic_lens" in props ? props.orthographic_lens : undefined;
-    const perspective_lens = !orthographic_lens ? _perspective_lens : undefined;
+    /**
+     * The properties used to create the camera entity in a dedicated state.
+     * This state can only be affected once per lifecycle to avoid recreating
+     * the entity on every render.
+     */
+    const [entityProps, setEntityProps] = useState<EntityProps>(null);
 
-    const { isPending, entity: cameraEntity } = useEntity(
-        {
-            name,
-            components: {
-                local_transform: { position, orientation, eulerOrientation },
-                camera: { renderGraphRef, dataJSON: settings, renderTargetIndex },
-                perspective_lens,
-                orthographic_lens,
-            },
-            options: { auto_broadcast: false, delete_on_client_disconnection: true },
-        },
-        watchedComponents,
-    );
+    //--------------------------------------------------------------------------
+    useEffect(() => {
+        if (!instance || !sceneSettings) {
+            return;
+        }
+
+        /* eslint-disable prefer-const */
+        let {
+            renderGraphRef: defaultRenderGraph,
+            dataJSON: defaultSettings,
+            renderTargetIndex: defaultRenderTargetIndex,
+        } = sceneSettings.default_camera_component;
+
+        if (defaultRenderGraph === INVALID_RENDER_GRAPH_UUID) {
+            defaultRenderGraph = DEFAULT_RENDER_GRAPH_UUID;
+        }
+
+        if (Object.keys(defaultSettings).length === 0) {
+            defaultSettings = DEFAULT_RENDER_GRAPH_SETTINGS;
+        }
+
+        let {
+            name = "Camera",
+            position = sceneSettings.default_camera_transform.position,
+            orientation,
+            eulerOrientation,
+            renderGraphRef = defaultRenderGraph,
+            settings = defaultSettings,
+            renderTargetIndex = defaultRenderTargetIndex,
+            perspective_lens,
+            orthographic_lens,
+        } = props;
+
+        if (eulerOrientation) {
+            orientation = undefined;
+        } else if (!orientation) {
+            orientation = sceneSettings.default_camera_transform.orientation;
+        }
+
+        if (orthographic_lens) {
+            perspective_lens = undefined;
+        } else if (!perspective_lens) {
+            perspective_lens = DEFAULT_PERSPECTIVE_LENS;
+        }
+
+        setEntityProps(
+            // Only set if not already set
+            prevValue =>
+                prevValue ?? {
+                    name,
+                    components: {
+                        local_transform: { position, orientation, eulerOrientation },
+                        camera: { renderGraphRef, dataJSON: settings, renderTargetIndex },
+                        perspective_lens,
+                        orthographic_lens,
+                    },
+                },
+        );
+    }, [instance, sceneSettings, props]);
+
+    //--------------------------------------------------------------------------
+    useEffect(() => {
+        if (!instance || !entityProps) {
+            return;
+        }
+
+        instance.scene
+            .newEntity({ ...entityProps, options: { auto_broadcast: false, delete_on_client_disconnection: true } })
+            .then(foundEntity => setCameraEntity(foundEntity))
+            .finally(() => setIsPending(false));
+
+        return (): void => {
+            setCameraEntity(null);
+            setIsPending(true);
+        };
+    }, [instance, entityProps]);
 
     return { isPending, cameraEntity };
 }
