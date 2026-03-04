f52d9282f056877736e6f5c61100c50ffa9f13fe
Alexandre Petit <alex@3dverse.com>
2026-03-04 15:17:27 -0500

    fix(livelink.react): viewports are now correctly released before the surfaces

--- livelink.react/sources/components/core/Viewport.tsx
+++ livelink.react/sources/components/core/Viewport.tsx
@@ -9,6 +9,7 @@ import React, {
     useCallback,
     useContext,
     useEffect,
+    useLayoutEffect,
     useRef,
     useState,
     useImperativeHandle,
@@ -139,7 +140,7 @@ export const Viewport = forwardRef(function (
 
     //--------------------------------------------------------------------------
     const zIndex = parentZIndex + 1;
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (!instance || !renderingSurface || !canvas || !viewportDomElement.current) {
             return;
         }
